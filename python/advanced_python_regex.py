87a559dc356c7a4738c201d58dec58941d4f4c6e
ryanglambert <ryan.g.lambert@gmail.com>
2015-12-30 20:26:31 -0800

    part 5a q1 - q4 complete

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import re
 import string
+import csv
 # import string << probly won't need
 
 def clean_acronym(s):
@@ -46,18 +47,57 @@ def get_title_counts(titles, title_filter):
         title_occurences[title] = num_occurences
     return title_occurences
 
+def read_file(file_path):
+    data = []
+    dictionary = {}
+    with open(file_path, 'rb') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            data.append(row)
+    header  = data[0]
+    body = data[1:]
+        
+    for i in range(len(header)):
+        column = []
+        for row in body:
+            column.append(row[i])
+        dictionary[header[i]] = column
+    return dictionary
+
 
 def main():
     faculty = pd.DataFrame.from_csv("~/repos/dsp/python/faculty.csv")
 ### Q1
+    print "############Question 1: Count of different degree types are:############\n".upper()
     degrees = faculty[' degree']
     unique_degrees = get_unique_degree(degrees)
     acronyms = generate_match_list(unique_degrees)
     print get_degree_counts(degrees, acronyms)
+    print "\n"
 ### Q2
+    print "############Question 2: Count of different faculty types is:############\n".upper()
     titles = faculty[' title']
     title_template = get_words_by_length(titles, length_atleast=4)
     print get_title_counts(titles, title_template)
+    print "\n"
+### Q3
+    print "############Question 3: list of email addresses############\n".upper()
+    faculty = read_file('faculty.csv')
+    emails = faculty[' email']
+    for i in emails: 
+        print i 
+    print "\n"
+### Q4
+    faculty = read_file('faculty.csv')
+    emails = faculty[' email']
+    email_domains = map(lambda x: re.findall(r'@.*', x), emails)
+    email_domains = [i[0] for i in email_domains]
+    email_domains = set(email_domains)
+
+    print "############Question 4: list of different email domains:############\n".upper()
+    for i in email_domains:
+        print i
+
 
 
 
