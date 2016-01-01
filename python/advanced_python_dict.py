72bdf2a80a4b68ee76da984ef4fba55a28310207
ryanglambert <ryan.g.lambert@gmail.com>
2015-12-31 22:40:14 -0800

    part5b Q7

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -26,9 +26,37 @@ def main():
 
     # for value in dictionary['Li']:
     #     print value
+    print "######### Question 6 ########"
     for i in range(3):
         print dictionary.keys()[i], str(dictionary[dictionary.keys()[i]]).strip("[]")
         
+# reset_dictionary for question 7 
+    dictionary = {}
+
+    with open('faculty.csv', 'rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            pdb.set_trace
+            try: 
+                dictionary[(row[0].split()[0], row[0].split()[-1])].append([
+                            row[1],
+                            row[2][:row[2].find("Professor") + len("Professor")], 
+                            row[3],
+                            ]
+                        )
+            except KeyError:
+                dictionary[(row[0].split()[0], row[0].split()[-1])] = [
+                        [
+                        row[1],
+                        row[2][:row[2].find("Professor") + len("Professor")], 
+                        row[3],
+                        ]
+                    ]
+    print "######### Question 7 ########"
+    for i in range(3):
+        print dictionary.keys()[i], str(dictionary[dictionary.keys()[i]]).strip("[]")
+
+
 
 
 if __name__ == '__main__':
