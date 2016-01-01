ef76b63145a8ba615cc6e65b472687e00e482dc1
ryanglambert <ryan.g.lambert@gmail.com>
2015-12-31 18:09:26 -0800

    part5a Q5 write email addresses complete

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,12 @@
-PLACE YOUR CODE HERE
+import advanced_python_regex  
+import csv
+
+def main():
+    faculty = advanced_python_regex.read_file('faculty.csv')
+    emails = faculty[' email']
+    with open('emails.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',')
+        writer.writerows([[i] for i in emails])
+
+if __name__ == "__main__":
+    main()
